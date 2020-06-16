93f1670186b4b70b557bbb490bcfe62d8c2922c6
reinaldocelis@outlook.com <reinaldocelis@outlook.com>
2020-06-16 15:23:40 -0400

    initial commit

---
+++ TriggerBeetrackJumbo/index.js
@@ -0,0 +1,16 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.data || (req.body && req.body.data)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.data || req.body.data)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+};
